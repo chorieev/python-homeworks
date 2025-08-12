006ab78294f0dde869192f2ec989fc84b442e36c
chorieev <westminster.bekhruz.707@gmail.com>
2025-08-12 07:01:56 +0500

    Create hw-1.py

---
+++ lesson-1/homework/hw-1.py
@@ -0,0 +1,37 @@
+# homework - 1
+
+# Given a side of square. Find its perimeter and area.
+
+a = 5 
+
+permimetr = 2 * a
+area = a*a
+print('Perimetr is: ' + str(permimetr) + ' sm')
+print('Area is: ' + str(area)  + ' sm')
+
+#  Given diameter of circle. Find its length.
+diametr  = 4
+pi = 3.14
+
+print('Diameter is: ' + str(diametr * pi) + ' sm')
+
+# Given two numbers a and b. Find their mean.
+
+a = 3 
+b = 4
+mean  = (a + b) / 2
+print ('a nd b"s mean is: ' + str(mean)) 
+
+# Given two numbers a and b. Find their sum, product 
+# and square of each number.
+
+a = 5
+b = 6
+sum = a + b
+product = a*b
+sq1 = a**2
+sq2 = b**2
+print('sum: ' + str(sum))
+print('product is: ' + str(product))
+print('Square of a: ' + str(sq1))
+print('Square of b: ' + str(sq2))
